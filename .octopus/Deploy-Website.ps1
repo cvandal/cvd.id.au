84c74a8c3b9c482d095b5bd9d6632b15a29375ac
Chris van Dal <chris.vandal@octopus.com>
2019-12-13 20:16:49 +1100

    temp show current dir

--- .octopus/Deploy-Website.ps1
+++ .octopus/Deploy-Website.ps1
@@ -4,6 +4,8 @@
 # Script Source: Script file in a package
 # Script Filename: .octopus/Deploy-Website.ps1
 
+dir
 Set-Location -Path ..
+dir
 $env:Path += ";$env:ProgramFiles\nodejs;$home\AppData\Roaming\npm"
 & firebase deploy --token $FIREBASE_TOKEN
