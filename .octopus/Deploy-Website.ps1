a566f6fc245b6a70fb3c8b6a7d0a1af73135efbe
Chris van Dal <chris.vandal@octopus.com>
2020-11-28 14:53:01 +1100

    getting ready for config as code

--- .octopus/Deploy-Website.ps1
+++
@@ -1,9 +0,0 @@
-# Step Template: Run a script
-# Step Name: Deploy cvd.id.au
-# Execution Location: On a worker from the default worker pool
-# Script Source: Script file in a package
-# Script Filename: .octopus/Deploy-Website.ps1
-
-Set-Location -Path ..
-$env:Path += ";$env:ProgramFiles\nodejs;$home\AppData\Roaming\npm"
-& firebase deploy --token $FIREBASE_TOKEN
--- .octopus/Install-Dependencies.ps1
+++
@@ -1,9 +0,0 @@
-# Step Template: Run a script
-# Step Name: Install Dependencies
-# Execution Location: On a worker from the default worker pool
-# Script Source: Script file in a package
-# Script Filename: .octopus/Install-Dependencies.ps1
-
-& choco install -y nodejs
-$env:Path += ";$env:ProgramFiles\nodejs"
-& npm install -g firebase-tools --no-optional --loglevel=error
